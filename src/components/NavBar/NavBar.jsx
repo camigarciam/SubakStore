3204f65aa9fad4de25f0a04cda199c2471d9e784
Camila Garcia <camizoegm@gmail.com>
2024-02-10 16:56:56 -0300

    deploy

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
   return (
     <Navbar expand="lg" className='navbar-container' variant="dark">
       <Container>
-        <Navbar.Brand as={Link} to="/" >
+        <Navbar.Brand as={Link} to="/SubakStore" >
         <img src="https://images.emojiterra.com/google/android-10/512px/1f349.png" height={30}></img>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
